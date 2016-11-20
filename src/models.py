5f66d86cf82759f99cb6edb260f1f8a8edff8888
Andrei Regiani <andrei.cpp@gmail.com>
2016-11-20 05:54:19 +0200

    Adjusted model values

--- src/models.py
+++ src/models.py
@@ -2,5 +2,5 @@ from mongoengine import *
 
 
 class ExampleModel(Document):
-    email = StringField(required=True, max_length=64)
-    name = StringField(required=True, max_length=64)
+    email = StringField(required=True, max_length=32)
+    name = StringField(required=True, max_length=16)
